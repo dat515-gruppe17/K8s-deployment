9594738e0aa1262230123b26b007a3a5360095ad
Carlme96 <carl.magnus.e@gmail.com>
2023-09-27 16:52:25 +0200

    ffew

--- dockerk8sSetup.bash
+++ dockerk8sSetup.bash
@@ -45,3 +45,5 @@ sudo kubectl get pods -n kube-flannel
 sudo kubectl get nodes -o wide
 sudo kubectl get nodes
 sudo kubectl taint nodes --all  node-role.kubernetes.io/control-plane-
+sudo kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
+sudo kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
---
+++ services.bash
@@ -0,0 +1,6 @@
+sudo kubectl apply -f proxy-deployment.yaml
+sudo kubectl apply -f proxy-service.yaml
+sudo kubectl apply -f backend-deployment.yaml
+sudo kubectl apply -f backend-service.yaml
+sudo kubectl apply -f frontend-deployment.yaml
+sudo kubectl apply -f frontend-service.yaml
