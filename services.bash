0fe0f5246d0098dd003f3cb52cbfe5e6b1a724b4
Carlme96 <carl.magnus.e@gmail.com>
2023-09-27 17:27:36 +0200

    ewffew

--- services.bash
+++ services.bash
@@ -1,3 +1,5 @@
+sudo kubectl apply -f proxy-deployment.yaml
+sudo kubectl apply -f proxy-service.yaml
 sudo kubectl apply -f backend-deployment.yaml
 sudo kubectl apply -f backend-service.yaml
 sudo kubectl apply -f frontend-deployment.yaml
